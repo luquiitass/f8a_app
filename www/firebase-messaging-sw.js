1e39f8e4af2d2905a39d2c2bde96fb722e21f316
lucas <liquiitass@gmail.com>
2021-09-21 13:51:02 -0300

    deploy app

--- www/firebase-messaging-sw.js
+++ www/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.16.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.8.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.8.0/firebase-messaging.js');
  
 firebase.initializeApp({
     apiKey: "AIzaSyBq60nK7Gidna-GTQZDx7tptj7uAR5rl6Y",
