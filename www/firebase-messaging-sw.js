7a7e5c15b20baed3845e3127bed9ddf7b3c1f6ec
lucas <liquiitass@gmail.com>
2021-09-21 12:11:57 -0300

    deploy app

---
+++ www/firebase-messaging-sw.js
@@ -0,0 +1,15 @@
+importScripts('https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.16.1/firebase-messaging.js');
+ 
+firebase.initializeApp({
+    apiKey: "AIzaSyBq60nK7Gidna-GTQZDx7tptj7uAR5rl6Y",
+    authDomain: "fu8a-10a3f.firebaseapp.com",
+    projectId: "fu8a-10a3f",
+    storageBucket: "fu8a-10a3f.appspot.com",
+    messagingSenderId: "381582243260",
+    appId: "1:381582243260:web:e983eee38074b69b80e7b9",
+    measurementId: "G-V9V61QWYM1"
+});
+ 
+const messaging = firebase.messaging();
+console.log('load firebase-messagiging-sw.js')
