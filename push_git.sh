e0afd5eacee5aa10b7ec7f1733af8f5644348f48
lucas <liquiitass@gmail.com>
2023-09-29 14:38:05 -0300

    deploy app

--- push_git.sh
+++ push_git.sh
@@ -1,1 +1,10 @@
-cd .. && ng build --prod && sleep 10s && cd prod/ && git add -A && git commit -m "deploy app" && git push origin futbol-alem;
+rm -r www && echo "eliminado www de rais" &&
+cd .. && echo "ubicando en directorio prod/" &&
+ng build --prod && echo " build prod" &&
+sleep 10s && echo "slep 10s" &&
+cp -r www prod/www && echo "copiando /www en /prod/www" &&
+sleep 10s && echo "slep 10s" &&
+cd prod/ && echo "abriendo dir /prod/" &&
+git add -A && echo "git add" &&
+git commit -m "deploy app" &&  echo "commit " && 
+git push origin futbol-alem && echo "push futbol-alem";
